9adc7a31a9697f801a5d4f597628a08236f14cb8
Codehack-01 <ademolaprojectdreams@gmail.com>
2024-04-06 09:38:29 +0100

    source code

--- src/components/ImageSearch.jsx
+++ src/components/ImageSearch.jsx
@@ -12,7 +12,7 @@ const ImageSearch = ({searchText}) => {
       <form id ='search' onSubmit={onSubmit} className="w-[90%] md:w-full max-w-sm ">
         <div className="flex items-center  border px-4 border-gray-500 py-2 rounded-[30px]">
           <input onChange={e => setText(e.target.value)} className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Search cars, nature..." />
-          <button className="flex-shrink-0 bg-black hover:bg-blue-400 hover:border-teal-700 text-sm text-white py-1 px-2 rounded" type="submit">
+          <button className="flex-shrink-0 bg-black hover:bg-black text-sm text-white py-1 px-2 rounded" type="submit">
             Search
           </button>
         </div>
